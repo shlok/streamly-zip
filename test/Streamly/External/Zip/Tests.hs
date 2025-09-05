c7f98767d36d3740e5ab5a6d3814da534d9b3835
Shlok Datye <shlok@quant.is>
2025-09-05 19:18:40 +0200

    Add a test that exposes runtime crashes.

--- test/Streamly/External/Zip/Tests.hs
+++ test/Streamly/External/Zip/Tests.hs
@@ -1,23 +1,32 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Streamly.External.Zip.Tests (tests) where
 
+import Control.Concurrent.Async
 import Control.Monad
 import qualified Crypto.Hash.SHA256 as SHA256
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Base16 as Base16
 import qualified Data.ByteString.Char8 as BC8
 import Data.Function
+import GHC.Conc
 import qualified Streamly.Data.Fold as F
 import qualified Streamly.Data.Stream.Prelude as S
 import Streamly.External.Zip
+import Streamly.External.Zip.Internal.Error
+import Test.QuickCheck.Monadic
 import Test.Tasty
 import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
 
 tests :: [TestTree]
 tests =
-  [testDataZip]
+  [ testDataZip,
+    testDataZipOneChunkConcurrent
+  ]
 
+-- | Reads all items from @data.zip@ and makes sure they are as expected.
 testDataZip :: TestTree
 testDataZip = testCase "testDataZip" $ do
   z <- openZip "test/data/data.zip" []
@@ -49,3 +58,34 @@ testDataZip = testCase "testDataZip" $ do
         ]
 
   assertEqual "" hashes expectedHashes
+
+-- |
+-- * Reads at most one chunk from each item in @data.zip@, numerous times concurrently.
+-- * Original motivation: reproduce unpredictable segfaults in version 0.0.1. We’re not fully
+--   certain we succeeded with the exact segfault we encountered (occurring for the same reasons),
+--   but we did produce some runtime crashes: at least @free(): invalid pointer@, @double free or
+--   corruption (fasttop),@ @double free or corruption (out)@, and silent runtime crashes. (Maybe
+--   those silent ones were @cabal test@ catching segfaults; see
+--   https://github.com/UnkindPartition/tasty/issues/354 for potential clues.)
+testDataZipOneChunkConcurrent :: TestTree
+testDataZipOneChunkConcurrent = testProperty "testDataZipOneChunkConcurrent" $ monadicIO $ do
+  assertEnoughCapabilities
+  numConcurrent <- pick $ chooseInt (1, 2 * numCapabilities)
+  run $ replicateConcurrently_ numConcurrent $ do
+    numEntries <- openZip "test/data/data.zip" [] >>= getNumEntries
+    forM_ [0 .. numEntries - 1] $ \idx -> do
+      z <- openZip "test/data/data.zip" []
+      _ <-
+        S.unfold @IO unfoldFileAtIndex (z, [], idx)
+          & S.fold F.one
+      return ()
+
+assertEnoughCapabilities :: (Monad m) => PropertyM m ()
+assertEnoughCapabilities =
+  when
+    (numCapabilities <= 1)
+    ( run $
+        throwError
+          "assertEnoughCapabilities"
+          "available threads <= 1; machine / Haskell RTS settings not useful for testing"
+    )
